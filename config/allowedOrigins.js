b69813a7bb1182b768f3249c234334529bc88b0e
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2024-06-26 23:56:31 +0530

    allowed origins added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,8 +3,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
-  "https://appointment-system-bice.vercel.app/",
+  "https://appointment-system-bice.vercel.app",
 ];
 
 module.exports = allowedOrigins;
-
